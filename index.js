4a7aa7835d7e8052e19be4e43c676834e3f2bcff
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-06 09:52:39 -0400

    commit tree 6494f84f5753cd1348ed8ab97d5a6aa360cb2d23

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function findWin(item) {
+    return item.result === "W"
+  }
+  
+  function superbowlWin(array) {
+    if( array.find(findWin)) {
+      return array.find(findWin).year;
+    }
+  }
+  
